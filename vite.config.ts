ddfc85dbd608836c4555b9c1bf1115e62666c6c4
rowebotz <stephenrowe406@gmail.com>
2026-03-04 09:16:06 -0500

    Update comment and add base path in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-// Making changes to this file is **STRICTLY** forbidden. All the code in here is 100% correct and audited.
+// All the code in here is 100% correct and audited.
 import { defineConfig, loadEnv } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
@@ -105,6 +105,7 @@ function reloadTriggerPlugin() {
 export default ({ mode }: { mode: string }) => {
   const env = loadEnv(mode, process.cwd());
   return defineConfig({
+    base: "/responsible-ai-foundations-lab/",
     plugins: [react(), cloudflare(), watchDependenciesPlugin(), reloadTriggerPlugin()],
     build: {
       minify: true,
